9a7973adb0312ed4111fdcbeb2f09ed911bd52bb
kns44 <56423475+kns44@users.noreply.github.com>
2020-04-05 21:58:16 +0700

    62070013 กานต์ณัฐ สุนพงษ์ศรี
    เปลี่ยนแปลง UI และแก้ไขบางส่วน

--- sd_life.c
+++ sd_life.c
@@ -2,48 +2,72 @@
 #include <math.h>
 #include <ctype.h>
 #include <time.h>
+#include<stdlib.h>
 
 void exit();
 void grade();
 void inex();
 void select();
-void fordelay(int a);
-
-int main(){
-    int choose;
-    printf("*=====* Welcome To Student Life Application *=====*\n");
-    printf("[1] Grade Calculator\n[2] Income-Expense\n[3] Exit\n");
-    printf("\nEnter Number To Continue: ");
+void main(){
+    int i;
+    printf("\n\n\n\n\n\t\t\t\t\xB2\xB2\xB2\xB2\xB2\xB2\xB2 Welcome To Student Life Application \xB2\xB2\xB2\xB2\xB2\xB2\xB2");
+    printf("\n\n\n\n\t\t\t\t\t\tLOADING");
+        for(i=0;i<=5;i++)
+        {
+            delay(100000000);
+            printf(" .");
+        }
+        printf("\n\n\n\n\t\t\t\t\t");
+        system("pause");
+        system("cls");
+        menu();
+}
+int menu(){
+    int choose, i;
+    printf("\n\n\n\t\t\t\t\xB2\xB2\xB2\xB2\xB2\xB2\xB2 Student Life Application \xB2\xB2\xB2\xB2\xB2\xB2\xB2\n");
+    printf("\n\t\t\t\t\t[1] Grade Calculator\n\t\t\t\t\t[2] Income-Expense\n\t\t\t\t\t[3]\n\t\t\t\t\t[0] Exit\n");
+    printf("\n\t\t\t\tEnter Number To Continue: ");
     scanf("%d", &choose);
     if(choose == 1){
+        printf("\n\t\t\t\t\tLOADING");
+        for(i=0;i<=3;i++)
+        {
+            delay(100000000);
+            printf(" .");
+        }
+        system("cls");
         grade();
     }
     else if(choose == 2){
         inex();
     }
+    else if(choose == 3){
+
+    }
     else if(choose == 0){
-        exit();
+
     }
 }
 void grade(){
     char all[50];
-    float subject, credit, ans, allgit, allans;
+    float subject, credit, ans, allgit, allans, last;
     int numof_g, counter = 0, i = 1;
-    printf("\n*=====* Welcome To Grade Calculator Function *=====*\n");
-    printf("Enter Number of Subject: ");
+    printf("\n\n\n\t\t\t\t\xB2\xB2\xB2\xB2\xB2\xB2\xB2 Grade Calculator Function \xB2\xB2\xB2\xB2\xB2\xB2\xB2\n");
+    printf("\n\t\t\t\t\t Enter Number of Subject: ");
     scanf("%d", &numof_g);
 
     while(counter!=numof_g){
-      printf("\nEnter Credit [%d]: ", i);
+      printf("\n\t\t\t\t\t%d. Enter Credit Of Subject Number [%d]: ", i, i);
       scanf("%f", &credit);
-      printf("ํYour Grade [%d]: ", i);
+      printf("\t\t\t\t\t   Enter Grade Of Subject Number [%d]: ", i);
       scanf("%f", &subject);
       ans = subject*credit;
       allgit += credit;
       allans += ans;
       counter+=1, i+=1;
     }
-    printf("\nYour GPA %.2f\n", allans/allgit);
+    last = allans/allgit;
+    printf("\n\t\t\t\t\tYour GPS is %.2f\n", last);
     select();
 }
 void inex(){
@@ -51,20 +75,18 @@ void inex(){
 }
 void select(){
     int end;
-    printf("\n[1] Back To Main Manu\n[2] Exit\n");
-    printf("\nEnter Number To Continue: ");
+    printf("\n\t\t\t\t\t\t[1] Back To Main Manu\n\t\t\t\t\t\t[2] Exit\n");
+    printf("\n\t\t\t\t\tEnter Number To Continue: ");
     scanf("%d", &end);
     if(end == 1){
-        main();
+        system("cls");
+        menu();
     }
-    if(end ==2){
-        exit();
-    }
-}
-void exit(){
+    else if(end ==2){
 
+    }
 }
-void fordelay(int a){
+void delay(int a){
     int i,k;
     for(i=0;i<a;i++)
          k=i;
